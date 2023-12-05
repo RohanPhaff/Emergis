a2e6e4db9e2e8c3d0d32537c66ae7079cf2e9c21
Jurgen van Fraeijenhove <frae0004@hz.nl>
2023-12-05 13:19:24 +0100

    Better factory

--- dashboard/database/factories/ProgramFactory.php
+++ dashboard/database/factories/ProgramFactory.php
@@ -18,9 +18,56 @@ class ProgramFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->unique()->word,
-            'description' => $this->faker->sentence,
+            'name' => $this->getFictitiousProgram(),
+            'description' => $this->getFictitiousProgramDescription(),
             'portfolio_holder' => users::all()->random()->name,
         ];
     }
+
+    private function getFictitiousProgram(): string
+{
+    static $counter = 0;
+
+    $programs = [
+        "Geestelijke Gezondheidszorg voor Gemeenschappen",
+        "Veerkacht en Herstelinitiatieven",
+        "Digitale Gezondheidsoplossingen",
+        "Preventieve Gezondheidszorgbevordering",
+        "Jongerenwelzijn en Ontwikkelingsprogramma's",
+        "Familieondersteuning en Welzijnsbevordering",
+        "Creatieve Therapie en Expressieve Behandelprogramma's",
+        "Traumageïnformeerde Zorguitbreiding",
+        "Ouderenwelzijn en Verbondenheid",
+        "Mindfulness en Gezinsgeoriënteerde Zorgbenaderingen",
+    ];    
+
+    $index = $counter % count($programs);
+    $counter++;
+
+    return $programs[$index];
+}
+
+private function getFictitiousProgramDescription(): string
+{
+    static $counter = 0;
+
+    $programDescriptions = [
+        "Programma dat zich richt op het bieden van geestelijke gezondheidszorgdiensten en -ondersteuning aan diverse gemeenschappen.",
+        "Initiatieven gericht op het bevorderen van veerkracht en herstel na diverse uitdagingen en traumatische ervaringen.",
+        "Ontwikkeling en implementatie van digitale oplossingen om de toegang tot gezondheidszorg te verbeteren en te innoveren.",
+        "Programma dat zich richt op preventieve maatregelen om gezondheidsproblemen te voorkomen en te verminderen.",
+        "Initiatieven gericht op het verbeteren van het welzijn en de ontwikkeling van jongeren door middel van diverse programma's.",
+        "Ondersteuningsprogramma's die gericht zijn op het bieden van hulp en welzijnsondersteuning aan families in verschillende situaties.",
+        "Programma dat creatieve therapieën en expressieve benaderingen gebruikt om herstel en behandeling te bevorderen.",
+        "Uitbreiding van zorg met aandacht voor traumatische ervaringen om betere ondersteuning te bieden aan hen die dit nodig hebben.",
+        "Initiatieven en programma's die zich richten op het welzijn, de verbondenheid en de ondersteuning van ouderen in de samenleving.",
+        "Gezinsgerichte zorgbenaderingen en mindfulnessprogramma's die het welzijn van gezinnen centraal stellen.",
+    ];
+     
+
+    $index = $counter % count($programDescriptions);
+    $counter++;
+
+    return $programDescriptions[$index];
+}
 }
--- dashboard/database/factories/ProjectFactory.php
+++ dashboard/database/factories/ProjectFactory.php
@@ -35,9 +35,9 @@ class ProjectFactory extends Factory
         }
 
         return [
-            'name' => $this->faker->word,
+            'name' => $this->getFictitiousProjectName(),
             'code' => $this->faker->bothify('##??#?'),
-            'description' => $this->faker->realText($maxNbChars = 255),
+            'description' => $this->getFictitiousProjectDescription(),
             'department' => \App\Models\Department::all()->random()->name,
             'man_hours' => $finalManHours,
             'budget' => $this->faker->numberBetween($min = 8000, $max = 20000),
@@ -51,7 +51,7 @@ class ProjectFactory extends Factory
             'initiator' => users::all()->random()->name,
             'actor' => users::all()->random()->name,
 
-            'reasoning' => $this->faker->realText($maxNbChars = 100),
+            'reasoning' => $this->getFictitiousProjectReasoning(),
             'uploaded_document_start' => $this->faker->optional()->text, // Assuming binary data is stored as text
             'uploaded_document_planning' => $this->faker->optional()->text, // Assuming binary data is stored as text
             'program' => Program::all()->random()->name,
@@ -61,5 +61,105 @@ class ProjectFactory extends Factory
             'check_discussion_RvB' => $this->faker->boolean,
         ];
     }
+
+    private function getFictitiousProjectName(): string
+{
+    static $counter = 0;
+
+    $projectNames = [
+        "Welzijnsbevordering in de Gemeenschap",
+        "Veerkacht in Herstelprogramma",
+        "Initiatief voor Virtuele Therapeutische Ruimtes",
+        "Geïntegreerd Toegankelijk Netwerk voor Geestelijke Gezondheidszorg",
+        "Empower360 Campagne voor Mentale Welzijn",
+        "Project Mindfulle Toekomst",
+        "Alliantie voor Ondersteuning door Lotgenoten",
+        "TeleZorg Verbinding: Oplossingen voor Zorg op Afstand",
+        "Gezonde Geesten Werkplek Programma",
+        "Jeugdige Weerbaarheidsopbouwers",
+        "Pilot Mindfulness in Onderwijs",
+        "Programma voor Navigatie van Familiewelzijn",
+        "Hub voor Creatieve Therapieën",
+        "Digitaal Centrum voor Geestelijke Gezondheid Hulpbronnen",
+        "Initiatief voor Gemeenschapsveerkrachtopbouw",
+        "Uitbreiding van Traumageïnformeerde Zorg",
+        "Project voor Welzijn en Verbinding bij Ouderen",
+        "Initiatief voor Mindful Ouderschap",
+        "Taskforce voor Crisisrespons en Herstel",
+        "Holistic Gezondheidsnavigatiesysteem",
+    ];
+
+    $index = $counter % count($projectNames);
+    $counter++;
+
+    return $projectNames[$index];
+}
+
+private function getFictitiousProjectDescription(): string
+{
+    static $counter = 0;
+
+    $projectDescriptions = [
+        "Verbetering van de gemeenschapsgezondheid door outreach-programma's en preventie-initiatieven.",
+        "Herstelprogramma gericht op veerkrachtopbouw en emotioneel welzijn.",
+        "Gebruik van virtuele ruimtes voor therapeutische behandelingen en ondersteuning.",
+        "Een toegankelijk netwerk voor geestelijke gezondheidszorgintegratie in verschillende disciplines.",
+        "Campagne gericht op empowerment en bewustwording van mentaal welzijn.",
+        "Een project dat zich richt op mindfulness en welzijn in de toekomst.",
+        "Ondersteuningsalliantie die peers helpt elkaar te ondersteunen in hun herstelreis.",
+        "Oplossingen voor zorg op afstand om toegankelijkheid te vergroten.",
+        "Initiatieven gericht op het bevorderen van gezonde geesten op de werkplek.",
+        "Programma voor het opbouwen van veerkracht bij jongeren en adolescenten.",
+        "Mindfulness in het onderwijs integreren om emotionele gezondheid te bevorderen.",
+        "Ondersteuningsprogramma voor families om hen te helpen bij het bevorderen van welzijn.",
+        "Creatieve therapieën gebruiken voor het bevorderen van herstel en expressie.",
+        "Digitaal platform voor toegang tot geestelijke gezondheidsbronnen en ondersteuning.",
+        "Opbouw van veerkracht binnen gemeenschappen voor langdurige welzijnsverbetering.",
+        "Uitbreiding van zorg die rekening houdt met traumatische ervaringen voor betere ondersteuning.",
+        "Welzijnsprogramma gericht op het verbinden van ouderen en bevordering van hun welzijn.",
+        "Initiatief om ouders te ondersteunen bij het ontwikkelen van mindful opvoedvaardigheden.",
+        "Taskforce die gericht is op crisisrespons en het bevorderen van herstel in gemeenschappen.",
+        "Geïntegreerd systeem voor gezondheidsnavigatie met focus op holistische zorgbenadering.",
+    ];
+
+    $index = $counter % count($projectDescriptions);
+    $counter++;
+
+    return $projectDescriptions[$index];
+}
+
+private function getFictitiousProjectReasoning(): string
+{
+    static $counter = 0;
+
+    $projectReasonings = [
+        "Het vergroten van preventieve zorginitiatieven zal de algehele gemeenschapsgezondheid verbeteren.",
+        "Het aanpakken van veerkrachtopbouw draagt bij aan een gezondere en sterkere gemeenschap.",
+        "Toegang tot online therapie en virtuele ondersteuning zal de zorgtoegankelijkheid vergroten.",
+        "Een geïntegreerd zorgnetwerk zal efficiëntere diensten en betere zorgcoördinatie bieden.",
+        "Bewustwordingscampagnes helpen bij het verminderen van het stigma rond geestelijke gezondheid.",
+        "Investeren in mindfulness en toekomstige welzijnsbenaderingen creëert robuustere ondersteuning.",
+        "Peer-supportsystemen stimuleren zelfredzaamheid en bieden essentiële ondersteuning.",
+        "Afstandszorgoplossingen vergroten de toegankelijkheid voor afgelegen gemeenschappen.",
+        "Gezonde geesten op de werkplek bevorderen productiviteit en welzijn van werknemers.",
+        "Het versterken van veerkracht bij jongeren leidt tot gezondere toekomstige generaties.",
+        "Mindfulness in het onderwijs helpt bij het ontwikkelen van emotionele intelligentie bij jongeren.",
+        "Ondersteuning van families draagt bij aan een ondersteunende thuisomgeving voor beter herstel.",
+        "Creatieve therapieën bieden een expressieve uitlaatklep voor individuele verwerking.",
+        "Digitale toegang tot bronnen bevordert laagdrempelige ondersteuning voor iedereen.",
+        "Gemeenschapsgerichte veerkracht bouwt een samenhangend ondersteuningssysteem op.",
+        "Het aanbieden van traumageïnformeerde zorg verbetert de kwaliteit van de behandeling.",
+        "Verbinding onder ouderen draagt bij aan een gevoel van gemeenschap en steun.",
+        "Ondersteuning van ouders met mindfulness bevordert gezinswelzijn en harmonie.",
+        "Snelle crisisresponsmechanismen bevorderen veerkracht en snel herstel.",
+        "Een holistische benadering verbetert de algehele gezondheid door integratie van zorgaspecten.",
+    ];
+
+    $index = $counter % count($projectReasonings);
+    $counter++;
+
+    return $projectReasonings[$index];
+}
+
 }
 
