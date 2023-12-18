d93a2a337980e10129b6232d2a4ff9fd09bf081c
Losat <phaf0001@hz.nl>
2023-12-18 15:29:14 +0100

    Create UserFactory.php

---
+++ dashboard/database/factories/UserFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Roles;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\users>
+ */
+class UsersFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+                 
+        ];
+    }
+}
