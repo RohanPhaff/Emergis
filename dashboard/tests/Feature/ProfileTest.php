abb5bdfb6d298d7517445cda2a481f811246904c
Losattieee <70520082+RohanPhaff@users.noreply.github.com>
2023-12-21 11:29:39 +0100

    Update ProfileTest.php

--- dashboard/tests/Feature/ProfileTest.php
+++ dashboard/tests/Feature/ProfileTest.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-$this->markTestSkipped('test is broken');
+static::markTestSkipped('test is broken');
 
 class ProfileTest extends TestCase
 {
